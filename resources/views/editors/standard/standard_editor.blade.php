d20a2a7ac8fdc0a3a9e99e4c577643f925a5980d
Darkan <office@darkan.me>
2017-12-08 07:17:19 +0100

    remove span id

--- resources/views/editors/standard/standard_editor.blade.php
+++ resources/views/editors/standard/standard_editor.blade.php
@@ -533,7 +533,6 @@ $stylesInputTextFile = get_data($url.'js/editors/standard/css/styles_inputtext.j
 						<% _.each(detailsCollection, function(item, i) {  %>
 							<li class="new-page-item-view new-page-item-add" item='<%= JSON.stringify( item ) %>' >
 								<img class="new-page-item-image-thumb" src="<%= item.src %>"/>
-								<span><%= item.pageId %><span>
 							</li>
 						<% }); %>	
 					</ul>
