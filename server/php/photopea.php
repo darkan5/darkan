6153c73313bac85004964517ab834efedbe3a955
darkan <t.wisniewski@rapsody.com.pl>
2018-01-07 22:35:57 +0100

    photopea

--- server/php/photopea.php
+++ server/php/photopea.php
@@ -10,7 +10,7 @@ $img = $_POST['p'];
 $img = str_replace('data:image/png;base64,', '', $img);
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
-$file = UPLOAD_DIR . uniqid() . '.png';
+$file =  'darkan.png';
 $success = file_put_contents($file, $data);
 print $success ? $file : 'Unable to save the file.';
 
