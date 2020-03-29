a45ed9c749ce11012db0ec26ed5f97626efc9578
root <root@debian-cloud.ops.oktawave.com>
2020-03-29 08:55:04 +0000

    package.json

--- script/node.sh
+++ script/node.sh
@@ -1,2 +1,2 @@
-pm2 delete all
-pm2 start /var/www/darkan/app/editors/standard/node_server/darkan_server.js --node-args="--expose-gc"
+sudo pm2 delete all
+sudo pm2 start /var/www/darkan/app/editors/standard/node_server/darkan_server.js --node-args="--expose-gc"
